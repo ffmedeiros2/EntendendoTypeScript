05ebd13ff982fd5d0e3c475c4cdbfec8c085186e
Felipe Medeiros <ffmedeiros2@gmail.com>
2021-03-09 21:45:07 -0300

    Utilizando Bibliotecas JS com TypeScript

---
+++ 11-bibliotecas/bibliotecas.ts
@@ -0,0 +1,10 @@
+// quando se utiliza esta solução o tsc para de realizar verificações na constante
+/* declare const $: any */
+
+// a melhor solução para utilizar o jquery é rodar o comando npm i --save-dev @types/jquery
+$('body').append('Usando jQuery');
+
+
+// links úteis
+// https://www.typescriptlang.org/docs/handbook/declaration-files/by-example.html
+// https://microsoft.github.io/TypeSearch/
---
+++ 11-bibliotecas/jquery.d.ts
@@ -0,0 +1,3 @@
+// esta solução também resolve o problema com o uso da biblioteca jquery, porém é incompleto
+// seria necessário declarar todos os métodos desta biblioteca
+/* declare const $: any */
